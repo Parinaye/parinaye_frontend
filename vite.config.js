3920b5c35ec72aed781dc0248883e278bd324246
AmarnathN <vayuteja@gmail.com>
2024-03-12 20:45:30 +0530

    update backend url to env

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,28 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path';
-import dotenv from 'dotenv'
-dotenv.config();
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    proxy: {
-      '/api': {
-        target: process.env.MY_BACKEND_URL,
-        secure: false,
+// export default defineConfig({
+  
+
+//   plugins: [react()],
+// })
+export default ({mode}) =>{
+  process.env = Object.assign(process.env, loadEnv(mode, process.cwd(), ''));
+  return defineConfig({
+    server: {
+      proxy: {
+        '/api': {
+          target: process.env.MY_BACKEND_URL,
+          secure: false,
+        },
+    }},
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
       },
-  }},
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
     },
-  },
-  plugins: [react()],
-})
+    plugins: [react()],
+});
+}
