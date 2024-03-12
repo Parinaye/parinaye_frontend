c1741db764218d9a6370bccc1268a22a6c43eab2
AmarnathN <vayuteja@gmail.com>
2024-03-12 18:35:20 +0530

    initial commit

---
+++ config/enums.config.js
@@ -0,0 +1,47 @@
+export const EDUCATION_ENUM = [
+  "undergraduate",
+  "graduate",
+  "masters",
+  "phd",
+  "10th",
+  "other",
+  "none",
+];
+export const MARITAL_STATUS_ENUM = [
+  "single",
+  "married",
+  "divorced",
+  "widowed",
+  "other",
+];
+export const ASSETS_ENUM = ["car", "house", "land", "other", "none"];
+export const PROFESSION_ENUM = ["job", "student", "business", "other", "none"];
+export const INCOME_ENUM = [
+  "< 5l",
+  "5l to 10l",
+  "10l to 15l",
+  "15l to 25l",
+  "> 25l",
+  "nil",
+];
+export const RELIGION_ENUM = ["hindu", "christian", "muslim", "other"];
+export const CASTE_ENUM = {
+  brahmin: ["saiva", "aacharya", "niyoogi", "nandavar", "vaidiki", "arvela"],
+  kamma: [
+    "gampa",
+    "illuvellani",
+    "gōdajāti",
+    "kāvali",
+    "vaduga",
+    "pedda",
+    "bangāru",
+    "chowdary",
+    "nāidu",
+  ],
+  kaapu: ["telaga", "balija", "ontari", "munnuru kapu", "turpu kapu"],
+  komati: ["vysya", "kalinga"],
+  reddy: ["ganta"],
+  yadav: ["golla", "yadava", "yadav"],
+  other: ["other"],
+};
+export const GENDER_ENUM = ["male", "female"];
