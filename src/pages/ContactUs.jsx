2708c1b955b1f0e2ebade1a63ac044fd8bc06316
AmarnathN <vayuteja@gmail.com>
2024-05-09 13:41:56 +0530

    Update contact us page

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -7,21 +7,22 @@ export default function ContactUs() {
       <Card className="w-full m-40 p-5 sm:w-1/2 ">
         <h1 className="text-4xl font-bold text-center p-4">Contact Us</h1>
         <div className="flex flex-col items-center gap-6">
-          <p>
-            <span className="text-primary">Phone :</span> 1234567890
-          </p>
+          <div className="flex flex-col items-center gap-1">
+            <span className="text-primary">Phone :</span> 
+            <p>+91 77993 32277</p>
+          </div>
 
-          <p>
+          <div className="flex flex-col items-center gap-1 ">
             <span className="text-primary">Email :</span>
-            <a href="mailto:Parinaye@gmail.com" className=" text-blue-500">
+            <a href="mailto:parinaye.matrimony@gmail.com" className=" text-blue-500">
               {" "}
-              Parinaye@gmail.com
+              parinaye.matrimony@gmail.com
             </a>
-          </p>
-          <p>
-            <span className="text-primary">Address :</span> 123, XYZ Street, ABC
-            City, 123456
-          </p>
+          </div>
+          <div className="flex flex-col items-center gap-1 ">
+            <span className="text-primary">Address :</span> 
+            <p>Chilakaluripeta, Guntur(Dt)<br/> Andhra Pradesh - 522 616</p>
+          </div>
         </div>
       </Card>
     </div>
